c9d2360881f32f0a1e7e4cd3eeff59e1f31e125c
James Graves <james.c.graves.jr@gmail.com>
2020-05-08 15:54:16 -0500

    Basic URL canonicalization.
    Just add https:// if not included for github.com and others,
    purely as a convenience to the user.

--- src/repos.rs
+++ src/repos.rs
@@ -5,12 +5,31 @@
 // use rusqlite::params;
 use anyhow::{Result, anyhow};
 
+static COMMON_HOSTING_SITES: [&str; 3] = [
+    "github.com",
+    "gitlab.com",
+    "bitbucket.com",
+];
+
+fn canonicalize_repo(repo: &mut String) {
+    for hosting_site in COMMON_HOSTING_SITES.iter() {
+        if repo.starts_with(hosting_site) {
+            repo.insert_str(0, "https://");
+        }
+    }
+}
 
 pub fn add_urls(urls: &[String]) -> Result<usize> {
     if urls.is_empty() {
         return Err(anyhow!("one or more URLs required"))
     }
-    println!("add {:?}", urls);
+
+    let mut m_urls = urls.to_owned();
+    for url in m_urls.iter_mut() {
+        canonicalize_repo(url);
+        println!("url {}", url);
+    }
+
     Ok(urls.len())
 }
 
@@ -18,10 +37,16 @@ pub fn remove_urls(purge: bool, urls: &[String]) -> Result<usize> {
     if urls.is_empty() {
         return Err(anyhow!("one or more URLs required"))
     }
+
+    let mut m_urls = urls.to_owned();
+    for url in m_urls.iter_mut() {
+        canonicalize_repo(url);
+        println!("url {}", url);
+    }
+
     if purge {
         println!("also purge");
     }
-    println!("remove {:?}", urls);
     Ok(urls.len())
 }
 
