bb9b025edd4c0bddf9450b5457ee68a1fa6ba1d3
James Graves <james.c.graves.jr@gmail.com>
2020-05-15 14:56:57 -0500

    Print error to stderr.

--- src/main.rs
+++ src/main.rs
@@ -96,7 +96,7 @@ fn main() {
             }
         }
         Err(err) => {
-            println!("error: {:?} ", err);
+            eprintln!("error: {:?} ", err);
             std::process::exit(1);
         }
     }
